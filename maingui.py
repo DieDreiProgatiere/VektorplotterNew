38e9646e4bce3461c0dafe888263a1d4e96b0ef1
Finn Brunke <69799368+finnbrunke@users.noreply.github.com>
2021-03-06 18:08:24 +0100

    Update maingui.py

--- maingui.py
+++ maingui.py
@@ -14,7 +14,12 @@ from matplotlib.backends.backend_tkagg import (
 # Implement the default Matplotlib key bindings.
 from matplotlib.backend_bases import key_press_handler
 from matplotlib.figure import Figure
-import numpy as np #Needed for testing of embedded matplotlib
+
+# Needed for testing of embedded matplotlib
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator
+import numpy as np 
 
 
 x = Vector3D(3, 4, 5)
@@ -70,9 +75,27 @@ class MainGUI(tk.Frame):
         self.canvasFrame.columnconfigure(1, weight = 1)
         self.canvasFrame.rowconfigure(1, weight = 1)
         ### Testing embedded matplotlib
-        fig = Figure(figsize=(5, 4), dpi=100)
-        t = np.arange(0, 3, .01)
-        fig.add_subplot(111).plot(t, 2 * np.sin(2 * np.pi * t))
+        fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+
+        # Make data.
+        X = np.arange(-5, 5, 0.25)
+        Y = np.arange(-5, 5, 0.25)
+        X, Y = np.meshgrid(X, Y)
+        R = np.sqrt(X**2 + Y**2)
+        Z = np.sin(R)
+
+        # Plot the surface.
+        surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+
+        # Customize the z axis.
+        ax.set_zlim(-1.01, 1.01)
+        ax.zaxis.set_major_locator(LinearLocator(10))
+        # A StrMethodFormatter is used automatically
+        ax.zaxis.set_major_formatter('{x:.02f}')
+
+        # Add a color bar which maps values to colors.
+        fig.colorbar(surf, shrink=0.5, aspect=5)
         ### Testing embedded matplotlib over
         self.canvas = FigureCanvasTkAgg(fig, master=self.canvasFrame)
         self.canvas.draw()
