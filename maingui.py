2c73e71ae78d5c4d240539f10e7161e4f12d3065
Finn Brunke <69799368+finnbrunke@users.noreply.github.com>
2021-03-13 17:28:29 +0100

    Update maingui.py

--- maingui.py
+++ maingui.py
@@ -1,6 +1,7 @@
-from PyQt5.QtWidgets import QApplication, QButtonGroup, QGridLayout, QGroupBox, QHBoxLayout, QLabel, QMainWindow, QPushButton, QVBoxLayout, QWidget
-from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QButtonGroup, QGridLayout, QGroupBox, QHBoxLayout, QLabel, QMainWindow, QPushButton, QScrollArea, QSizePolicy, QVBoxLayout, QWidget, QLineEdit
 from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtCore import Qt, QSize
+from PyQt5 import QtWidgets, uic
 import sys
 import plotly
 import plotly.express as px
@@ -19,6 +20,8 @@ from colorAssignclass import ColorAssign
 x = Vector3D(3, 4, 5)
 y = Plane.normalForm(Vector3D(1, 1, 1), Vector3D(2, 2, 2))
 z = Plane.parameterForm(Vector3D(3, 4, 5), Vector3D(2, 2, 2), Vector3D(2, 2, 2))
+u = Plane.parameterForm(Vector3D(2, 5, 5), Vector3D(1, 4, 2), Vector3D(2, 6, 7))
+v = Plane.coordinateForm(Vector3D(3, 2, 1), 2)
 
 class MainWindow(QMainWindow):
 
@@ -32,7 +35,10 @@ class MainWindow(QMainWindow):
         self.width = 1000
         self.height = 700
         self.setGeometry(self.x, self.y, self.width, self.height)
-        self.setLayout(QGridLayout())
+        self.mainWidget = QWidget()
+        self.mainLayout = QGridLayout()
+        self.mainWidget.setLayout(self.mainLayout)
+        self.setCentralWidget(self.mainWidget)
         self.main()
 
     def main(self):
@@ -48,46 +54,61 @@ class MainWindow(QMainWindow):
 
 
     def makeWebEngineView(self, fig):
-        self.webBox = QWidget(self)
-        self.webBox.setLayout(QHBoxLayout())
+        self.webBox = QWidget()
+        self.webBoxLayout = QHBoxLayout()
         self.html = '<html><body>'
         self.html += plotly.offline.plot(fig, output_type='div', include_plotlyjs='cdn')
         self.html += '</body></html>'
 
         self.plot_widget = QWebEngineView()
         self.plot_widget.setHtml(self.html)
-        self.webBox.layout().addWidget(self.plot_widget) 
-        
-        self.layout().addWidget(self.webBox)
+        self.webBoxLayout.addWidget(self.plot_widget)
+
+        self.webBox.setLayout(self.webBoxLayout)
+        self.mainLayout.addWidget(self.webBox, 1, 1)
 
     def makeListView(self):
         self.listBox = QWidget()
-        self.listBox.setLayout(QGridLayout())
+        self.listBoxLayout = QGridLayout()
+        self.listBox.setMaximumHeight(700)
+        self.listBox.setMaximumWidth(500)
+        self.listBox.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.listScroll = QScrollArea()
+        self.listScroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.listScroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.listScroll.setWidgetResizable(True)
+        self.listScroll.setMaximumWidth(500)
+        self.listScroll.setWidget(self.listBox)
+
         self.listLabel = QLabel(self.listBox)
         self.listLabel.setText("List:")
-        self.listBox.layout().addWidget(self.listLabel, 0, 0)
+        self.listBoxLayout.addWidget(self.listLabel, 0, 0)
         for element, index in zip(ObjectLists.getObjDict(), range(ObjectLists.getObjDictLen())):
             self.objButton = QPushButton(self.listBox)
-            self.objButton.setText(str(element) + str(ObjectLists.getObjDict()[element]))
+            self.objButton.setText(str(element) + ": " + str(ObjectLists.getObjDict()[element]))
             self.objButton.adjustSize()
             self.objButton.clicked.connect(lambda: self.clicked()) # clicked is Placeholder, lambda for future args
             self.objButton.move(0, 25 + index * 25)
-            self.listBox.layout().addWidget(self.objButton, index + 1, 0)
+            self.objButton.setMinimumSize(QSize(50, 50))
+            self.objButton.setMaximumWidth(500)
+            self.listBoxLayout.addWidget(self.objButton, index + 1, 0)
             self.listBox.update()
 
-        self.layout().addWidget(self.listBox)
+        self.listBox.setLayout(self.listBoxLayout)
+        self.mainLayout.addWidget(self.listScroll, 1, 0)
 
     def makeMenuView(self):
         self.menuBox = QWidget()
-        self.menuBox.setLayout(QHBoxLayout())
+        self.menuBoxLayout = QHBoxLayout()
         self.homeButton = QPushButton()
         self.homeButton.setText("Home")
         self.homeButton.adjustSize()
         self.homeButton.clicked.connect(lambda: self.home()) # home is Placeholder, lambda for future args
         self.homeButton.move(0, 0)
-        self.menuBox.layout().addWidget(self.homeButton, 0)
+        self.menuBoxLayout.addWidget(self.homeButton, 0)
 
-        self.layout().addWidget(self.menuBox)
+        self.menuBox.setLayout(self.menuBoxLayout)
+        self.mainLayout.addWidget(self.menuBox, 0, 0)
 
 
     
